b2ca9c5f6b97a94885fe57335c60e8995b599e7e
KDiaCodes <KDiaCodes@users.noreply.github.com>
2021-10-16 10:05:31 +0200

    Update ou3.c

--- ou3.c
+++ ou3.c
@@ -12,3 +12,83 @@ typedef struct {
 
 void initField (const int rows, const int cols, cell field[rows][cols]);
 void clearField(const int rows, const int cols, cell field[rows][cols]);
+void loadGlider(const int rows, const int cols, cell field[rows][cols]);
+void loadSemaphore(const int rows, const int cols, cell field[rows][cols]);
+void loadRandom(const int rows, const int cols, cell field[rows][cols]);
+void loadCustom(const int rows, const int cols, cell field[rows][cols]);
+char getStartStateChoice(void);
+
+int main(void){
+  return 0;
+}
+
+void initField(const int rows, const int cols, cell field[rows][cols]) {
+  char choice;
+  clearField(rows, cols, field);
+  choice = getStartStateChoice();
+  
+  switch (choice){
+    case 'g':
+    case 'G':
+      loadGlider(rows, cols, field);
+      break;
+    case 's':
+    case 'S':
+      loadSemaphore(rows, cols, field);
+      break;
+    case 'r':
+    case 'R':
+      loadRandom(rows, cols, field);
+      break;
+    case 'c':
+    case 'C':
+    default:
+      loadCustom(rows, cols, field);
+      break;
+  }
+}
+char getStartStateChoice(void){
+  int ch;
+  //shouldnt ch be a char?
+  
+  printf("Select field spec to load ([G]lider, [S]emaphore, [R]andom ");
+  printf("or [C]ustom): ");
+  
+  ch = getchar();
+  if (ch != '\n') {
+    getchar();
+  }
+  return ch;
+}
+
+void cleadField(const int rows, const int cols, cell field[rows][cols]){
+  for (int r = 0; r < rows; r++) {
+    for (int c = 0; c < cols; c++) {
+      field[r][c].current = DEAD;
+    }
+  }
+}
+void loadGlider(const int rows, const int cols, cell field[rows][cols]){
+  field[0][1].current = ALIVE;
+  field[1][2].current = ALIVE;
+  field[2][0].current = ALIVE;
+  field[2][1].current = ALIVE;
+  field[2][2].current = ALIVE;
+}
+void loadSemaphore(const int rows, const int cols, cell field[rows][cols]){
+  field[8][1].current = ALIVE;
+  field[8][2].current = ALIVE;
+  field[8][3].current = ALIVE;
+}
+void loadRandom(const int rows, const int cols, cell field[rows][cols]){
+  
+}
+void loadCustom(const int rows, const int cols, cell field[rows][cols]){
+  int r, c;
+  
+  printf("Give custom format string: ");
+  do {
+    scanf("%d,%d", &r, &c);
+    field[r][c].current = ALIVE;
+  } while (getchar() != '\n');
+}
