5c5a45ccfb882225722f05f0d0292ffd04a394aa
KDiaCodes <KDiaCodes@users.noreply.github.com>
2021-10-21 22:40:19 +0200

    Update ou2.c

--- ou2.c
+++ ou2.c
@@ -1,6 +1,3 @@
-//namn: Samuel Kenrick
-//cs-id: oi21skk
-
 #include <stdio.h>
 
 //Start function that will say hello to the user. 
@@ -13,90 +10,86 @@ void start (void){
 //Function to ask the user how many judges they would like.
 int judgeQuery (){
   int userInput;
+
   do{
     printf("Number of judges (min 3 and max 10 judges)? ");
     scanf("%d", &userInput);
   } while(userInput > 10 || userInput < 3);
+
   return userInput;
 }
 
-//function that returns an array of scores to main() 
-float *readScores(float *arr, int nrJudges) {
+/*
+function that returns an array of the scores to main() 
+i use the array name "arr" to symbolise that im using
+the array arr passed to the function.
+ */
+void readScores(float *arr, int nrJudges) {
   for (int i = 1; i <= (nrJudges); i++){
     printf("Score from judge %d? ", i);
     scanf("%f", &arr[i-1]);
   }
-  return arr;
 }
 
 //function that prints out each judge's score
-void printScores (float *n, int nrJudges) {
+void printScores (float *arr, int nrJudges) {
   printf("\nLoaded scores:\n");
+
   for (int i = 1; i <= (nrJudges); i++) {
-    printf("Judge %d: %.1f \n", i, n[i-1]);
+    printf("Judge %d: %.1f \n", i, arr[i-1]);
   }
 }
 
-//Bubble sort to sort the array in ascending order
-float *sortArray (float *a, int nrJudges) {
-  int i, j;
-  float temp;
-  int Swap = 1;
-  while (j < nrJudges && Swap == 1){
-    Swap = 0;
-    for (i = 0; i < (nrJudges-1); i++){
-      if (a[i] > a[i+1]) {
-        temp = a[i];
-        a[i] = a[i+1];
-        a[i+1] = temp;
-        Swap = 1;
-      }
+//function to find the highest/lowest number and print the average (not accounting for largest or lowest)
+void results(float *arr, int nrJudges, float *min, float *max, float *avg) {
+  float sum;
+
+  *min = arr[0];
+  *max = arr[0];
+  for (int i = 1; i < nrJudges; i++){
+    if (arr[i]<*min) {
+      *min = arr[i];
     }
-    j++;
-  }  
-  return a;
+    if (arr[i]>*max) {
+      *max = arr[i];
+    }
+  }
+
+  for (int i = 0; i < (nrJudges); i++) {
+    sum = sum + arr[i];
+  } 
+
+  *avg = (sum - (*min + *max))/(nrJudges - 2); 
 }
 
-//function to first aquire the total sum of the scores (not including the largest and smallest) and print the average
-void averageWeight(float *p, int nrJudges) {
-  printf("Highest judge score: %.1f\n", p[nrJudges-1]);
-  printf("Lowest judge score: %.1f\n", p[0]);
-  float sum = 0;
-  for (int i = 1; i <= (nrJudges-2); i++) {
-    sum = sum + p[i];
-  } 
-  printf("Final average score: %.1f \n", sum/(nrJudges-2));
+void printResults(float min, float max, float avg) {
+  printf("\nFinal result:\n");
+  printf("Highest judge score: %.1f\n", max);
+  printf("Lowest judge score: %.1f\n", min);
+  printf("Final average score: %.1f \n", avg);
 }
 
 int main (void) {
   int nrJudges;
-  
   //calling the start function to print the beginning text
   start();
-
   //setting nrJudges to the return value of the JudgeQuery function
   nrJudges = judgeQuery();
-
-  printf("\n");
-  
-  float *n;
+  printf("\n"); 
+  //float *n;
   float arr[nrJudges];
   //setting the array n to the returning array from readScores
-  n = readScores(arr, nrJudges);
+  readScores(arr, nrJudges);
 
   //printing the scores contained in n
-  printScores(n, nrJudges);
+  printScores(arr, nrJudges);
+  
 
-  //sorting the array in ascending order -
-  float *p;
-  p = sortArray(n, nrJudges);
-  
-  printf("\nFinal result:\n");
-  
+  float min, max, avg;
   //calling for the average, not accounting for the largest and smallest scores
-  averageWeight(p, nrJudges);
+  results(arr, nrJudges, &min, &max, &avg);
+  printResults(min, max, avg);
 
   //end main()
   return 0;
-
 }
