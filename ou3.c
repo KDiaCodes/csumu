5c89e981169215ed367f5fa8b8383817923507b6
KDiaCodes <KDiaCodes@users.noreply.github.com>
2021-10-21 12:05:45 +0200

    Update ou3.c

--- ou3.c
+++ ou3.c
@@ -16,6 +16,8 @@ typedef struct {
 void initField(const int rows, const int cols, cell field[rows][cols]);
 void printGame(const int rows, const int cols, cell field[rows][cols]);
 void nextGen(const int rows, const int cols, cell field[rows][cols]);
+int checkNeighbours(const int rows, const int cols, cell field[rows][cols], int currentRow, int currentCol);
+void updateCurrent(const int rows, const int cols, cell field[rows][cols]);
 void clearField(const int rows, const int cols, cell field[rows][cols]);
 void loadGlider(const int rows, const int cols, cell field[rows][cols]);
 void loadSemaphore(const int rows, const int cols, cell field[rows][cols]);
@@ -41,26 +43,22 @@ int main(void){
   initField(rows, cols, field);
   
   // defining a variable to keep our loop active for displaying future generations
-  int exit = 0;
-
-  //defining a while loop we want to keep running as long as the user asks for the next generation.
-  while (exit == 0) {
+  char ch;
+  //defining a do while loop we want to keep running as long as the user asks for the next generation.
+  do {
     
     // printing this generation and setting the next one
     printGame(rows, cols, field);
     nextGen(rows, cols, field);
+    updateCurrent(rows, cols, field);
     
     // asking whether to print a next generation or not
     printf("Select one of the following options:\n");
     printf("(enter) Step\n");
     printf("(any) Exit\n");
-    char ch = (char)getchar();
+    ch = (char)getchar();
 
-    // checking if the person chose to continue or not
-    if (ch != '\n'){
-      exit = 1;
-    }
-  }
+  } while (ch == '\n');
 
   return 0;
 }
@@ -89,60 +87,77 @@ void printGame(const int rows, const int cols, cell field[rows][cols]) {
  * Input:       rows - the number of rows in the field
  *              cols - the number of columns in the field
  *              field - the field array
- * Output:      The field array's current values change to the next generation's.
+ * Output:      The field array's next values are determined.
  */
 void nextGen(const int rows, const int cols, cell field[rows][cols]){
   
   // defining a variable to keep track of how many neighbours are alive
-  int liveNeighbours = 0;
-
+  int liveNeighbours;
   // these two for loops are to go through our entire field position by position.
-  for (int k = 0; k < rows; k++) {
-    for (int l = 0; l < cols; l++) {
+  for (int currentR = 0; currentR < rows; currentR++) {
+    for (int currentC = 0; currentC < cols; currentC++) {
 
-      // resetting the counter
-      liveNeighbours = 0;
-
-      /* these two for loops check a 3x3 section in our array with k and l as the centre position
-      *  the localR variable checks for the current row, above row and below row for the current position
-      *  the localC variable checks for the current column, above column and below column for the current position
-      */
-      for (int localR = k-1; localR <= (k+1); localR++) {
-        for (int localC = l-1; localC <= (l+1); localC++) {
-
-          // if the square we are checking is outside the array or is the centre-piece we skip it
-          if (localR < 0 || localC < 0 || localR >= rows || localC >= cols || (localR == k && localC == l)) continue;
-
-          // if its a valid square and it is alive, add 1 to liveNeighbours
-          if (field[localR][localC].current == ALIVE)liveNeighbours++;
-        }
-      }
+      
+      liveNeighbours = checkNeighbours(rows, cols, field, currentR, currentC);
       
       // the conditions for whether to change the current square's value
-      if (liveNeighbours <= 1) {
-        field[k][l].next = DEAD;
-      } else if ((liveNeighbours == 2) && (field[k][l].current == ALIVE)) {
-        field[k][l].next = ALIVE;
-      } else if ((liveNeighbours == 2) && (field[k][l].current == DEAD)) {
-        field[k][l].next = DEAD;
-      } else if (liveNeighbours == 3) {
-        field[k][l].next = ALIVE;
-      } else if (liveNeighbours >= 4) {
-        field[k][l].next = DEAD;
+      switch (liveNeighbours) {
+        case 2:
+          field[currentR][currentC].next = field[currentR][currentC].current;
+          break;
+        case 3:
+          field[currentR][currentC].next = ALIVE;
+          break;
+        default:
+          field[currentR][currentC].next = DEAD;
       }
-      
     }
   }
+}
 
-  // setting each square to its new value simultaneously
+/* Function:    checkNeighbours
+ * Description: checks how many of the closest neighbours are alive
+ *              and returns the amount to the nextGen funtction 
+ * Input:       rows - the number of rows in the field
+ *              cols - the number of columns in the field
+ *              field - the field array
+ *              currentRow - the current positions row value
+ *              currentCol - the current positions column value
+ * Output:      The field array's current values change to the next generation's.
+ */
+int checkNeighbours(const int rows, const int cols, cell field[rows][cols], int currentRow, int currentCol){
+  int liveNeighbours = 0;
+
+  /* these two for loops check a 3x3 section in our array with k and l as the centre position
+  *  the localR variable checks for the current row, above row and below row for the current position
+  *  the localC variable checks for the current column, above column and below column for the current position
+  */
+  for (int localR = currentRow-1; localR <= (currentRow+1); localR++) {
+    for (int localC = currentCol-1; localC <= (currentCol+1); localC++) {
+
+      // if the square we are checking is outside the array or is the centre-piece we skip it
+      if (localR < 0 || localC < 0 || localR >= rows || localC >= cols || (localR == currentRow && localC == currentCol)) continue;
+      
+      // if its a valid square and it is alive, add 1 to liveNeighbours
+      if (field[localR][localC].current == ALIVE)liveNeighbours++;
+    }
+  }
+  return liveNeighbours;
+}
+
+/* Function:    updateCurrent
+ * Description: updates all the values of each position to the next generation's simultaneously
+ * Input:       rows - the number of rows in the field
+ *              cols - the number of columns in the field
+ *              field - the field array
+ * Output:      The field array's current values change to the next generation's.
+ */
+void updateCurrent(const int rows, const int cols, cell field[rows][cols]){
   for (int r = 0; r < rows; r++) {
     for (int c = 0; c < cols; c++) {
       field[r][c].current = field[r][c].next;
     }
   }
-
-
-
 }
 
 /* Function:    initField
@@ -259,9 +274,8 @@ void loadRandom(const int rows, const int cols, cell field[rows][cols]){
   for (int p = 0; p < rows; p++) {
     for (int q = 0; q < cols; q++) {
       
-      // resetting the random number and generating either the number 1 or 0
-      int state = 0;    
-      state = (rand() & 1);
+      // resetting the random number and generating either the number 1 or 0    
+      int state = (rand() & 1);
 
       // if the number is 1 set the position to ALIVE. Otherwise set to DEAD.
       if (state == 1) {
